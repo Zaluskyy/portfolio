a533e100b00f0f62c66bff231fe957ea76b19061
Krystian Zaluski <kryzal77@gmail.com>
2021-02-20 20:19:54 +0100

    cloud scrollbr fix

---
+++ src/components/Projects.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../style/projects.css';
+
+const Projects = () => {
+    return (
+        <div className="projects">
+            <font>You can see my fucking projects on my github</font>
+        </div>
+    );
+}
+ 
+export default Projects;
---
+++ src/components/Skills.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../style/skills.css'
+
+const Skills = () => {
+    return (
+        <div className="skills">
+            dsfds
+        </div>
+    );
+}
+ 
+export default Skills;
