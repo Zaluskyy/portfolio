0e0561914a4da1635b2bcc017a1c4a4f73ccaeac
Zaluskyy <70617924+Zaluskyy@users.noreply.github.com>
2024-02-24 00:42:27 +0100

    add animation to h1 in home

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -1,7 +1,9 @@
 "use client";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import style from "./style/Home.module.scss";
 import Image from "next/image";
+
+import { motion } from "framer-motion";
 
 import logoIcon from "../../../public/icon/logo.svg";
 
@@ -26,6 +28,79 @@ interface HomeProps {}
 const Home: React.FC<HomeProps> = ({}) => {
   const context = useContext(PortfolioContext);
   const { pageWidth } = context;
+
+  const random = () => Math.random() * 200;
+
+  const nameVariant = {
+    initial: {
+      x: random(),
+      y: random(),
+      opacity: 0,
+    },
+    animate: (id: number) => ({
+      x: 0,
+      y: 0,
+      opacity: 1,
+      scale: [0, 0.1, 0.5, 0.1, 1],
+      transition: {
+        duration: 0.1 * id + 1,
+      },
+    }),
+  };
+
+  const randomFromTo = (
+    fromX: number,
+    toX: number,
+    fromY: number,
+    toY: number
+  ) => {
+    const randomX = Math.random() * (toX - fromX) + fromX;
+    const randomY = Math.random() * (toY - fromY) + fromY;
+
+    return { x: randomX, y: randomY };
+  };
+
+  const randomArrF: any[] = [];
+  for (let i = 0; i < 23; i++) {
+    randomArrF.push(randomFromTo(-300, 300, -400, 400));
+  }
+  const [randomArr] = useState<any[]>(randomArrF);
+
+  const nameArr = ["Front-end", "React", "Developer"];
+
+  const charName = nameArr.map((item, index) => {
+    return item.split("").map((char, charIndex) => {
+      let id: number = 0;
+      if (index == 0) id = charIndex;
+      else if (index == 1) id = charIndex + nameArr[index - 1].length;
+      else if (index == 2)
+        id = charIndex + nameArr[index - 1].length + nameArr[index - 2].length;
+      return (
+        <motion.span
+          key={id}
+          variants={nameVariant}
+          initial={{ x: randomArr[id]?.x, y: randomArr[id]?.y, opacity: 0 }} //tu jest błąd
+          animate="animate"
+          custom={id}
+          drag
+          dragConstraints={{ left: 0, top: 0, right: 0, bottom: 0 }}
+          dragElastic={0.8}
+        >
+          {char}
+        </motion.span>
+      );
+    });
+  });
+
+  const name = charName.map((item, index) => {
+    return (
+      <div key={index}>
+        <span className={style.word}>{item}</span>
+        <span> </span>
+      </div>
+    );
+  });
+
   const techArr = [
     { icon: htmlIcon, name: "html" },
     { icon: cssIcon, name: "css" },
@@ -49,7 +124,8 @@ const Home: React.FC<HomeProps> = ({}) => {
             <Image src={logoIcon} alt="logo" />
           </div>
           <div className={style.imgContainer}>
-            {pageWidth ? (
+            {/* daj tutaj potem window ale z useeffectu bo coś się kurwa spierdala chuj jebany kurwa */}
+            {/* {pageWidth ? (
               pageWidth < 820 ? (
                 <Image priority={true} src={meSquareImg} alt="Krystian" />
               ) : (
@@ -59,15 +135,14 @@ const Home: React.FC<HomeProps> = ({}) => {
               <Image priority={true} src={meSquareImg} alt="Krystian" />
             ) : (
               <Image priority={true} src={meImg} alt="Krystian" />
-            )}
+            )} */}
           </div>
         </div>
       </div>
 
       <div className={style.left}>
         <div className={style.center}>
-          <h1>Front-end React developer</h1>
-
+          <h1>{name}</h1>
           <h3>
             Hi, I’m Krystian Załuski. Front-end React Developer from Poland,
             Warsaw
